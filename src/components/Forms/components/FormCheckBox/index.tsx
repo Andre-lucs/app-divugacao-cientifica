29a966ee652233b821a72812827a7bf308062a89
Andre-lucs <andreltd312@gmail.com>
2024-08-01 08:38:23 -0300

    feat: formComponent v1

--- src/components/Forms/components/FormCheckBox/index.tsx
+++ src/components/Forms/components/FormCheckBox/index.tsx
@@ -1,7 +1,6 @@
-import CheckBox from 'expo-checkbox';
 import { PropsWithChildren, useState } from 'react';
 import { Text } from 'react-native';
-import { FormCheckBox } from '@/src/components/Forms/styles';
+import { FormCheckBox, CheckBox } from '@/src/components/Forms/styles';
 import { FormInputProps } from '../../types';
 
 type props = FormInputProps & PropsWithChildren<{
@@ -14,7 +13,7 @@ export default function(p:props){
 
   return(
     <FormCheckBox>
-      <CheckBox value={checked} onValueChange={setChecked}  />
+      <CheckBox value={checked} color={checked ? "#5470FF":''} onValueChange={setChecked}  />
       <Text>{p.children}</Text>
     </FormCheckBox>
   )
