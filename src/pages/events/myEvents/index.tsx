97fde1e81c25226b88bf103e516a9f11f5e63e94
Diogo Santana <diogosantanafreitasuna@gmail.com>
2024-09-09 21:09:54 -0300

    feat: adiccionando página de 'Meus Eventos'

---
+++ src/pages/events/myEvents/index.tsx
@@ -0,0 +1,28 @@
+import { Alert, Button, Text, View, TextInput, StyleSheet, ScrollView } from "react-native";
+import BackButton from "@/src/components/BackButton";
+import SearchInput from "@/src/components/SearchInput";
+import EventPreview from "@/src/components/Events/EventPreview";
+import { ContainerPageEvents, HeaderPageEvents, TitlePageEvents } from "..";
+
+
+export default function () {
+
+
+  return (
+    <ContainerPageEvents>
+      <HeaderPageEvents>
+        <BackButton/>
+        <TitlePageEvents>Meus Eventos:</TitlePageEvents>
+        <SearchInput/>
+      </HeaderPageEvents>
+      <ScrollView>
+        <EventPreview category="IA" date="09/09/2024" name="SBC" imageUrl={"https://images.unsplash.com/photo-1518773553398-650c184e0bb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+        <EventPreview category="IA" date="09/09/2024" name="SBC" imageUrl={"https://images.unsplash.com/photo-1518773553398-650c184e0bb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+        <EventPreview category="IA" date="09/09/2024" name="SBC" imageUrl={"https://images.unsplash.com/photo-1518773553398-650c184e0bb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+        <EventPreview category="IA" date="09/09/2024" name="SBC" imageUrl={"https://images.unsplash.com/photo-1518773553398-650c184e0bb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+        <EventPreview category="IA" date="09/09/2024" name="SBC" imageUrl={"https://images.unsplash.com/photo-1518773553398-650c184e0bb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+        <EventPreview category="IA" date="09/09/2024" name="SBC" imageUrl={"https://images.unsplash.com/photo-1518773553398-650c184e0bb3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+      </ScrollView>
+    </ContainerPageEvents>
+  );
+}
