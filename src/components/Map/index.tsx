9f6044e728080c835bcca460141059b3f11a7e81
Diogo Santana <diogosantanafreitasuna@gmail.com>
2024-09-12 23:08:32 -0300

    feat: adicioando componente de Mapa

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+
+export default function Map() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',  
+    height: 300, 
+
+    marginBottom: 100
+  },
+  map: {
+    flex: 1, 
+  },
+});
