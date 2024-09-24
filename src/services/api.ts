16538b97339584c56eeedff28e67fc2887b88953
Andre-lucs <andreltd312@gmail.com>
2024-09-24 20:44:04 -0300

    feat: added FormData suport in fetchApi

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@ import { AuthContextDataType } from "@/@types/authTypes";
 import { PORT, SERVER_IP } from "@/globalVariables";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import { usePathname, useRouter } from "expo-router";
 
 interface FetchDataOptions {
   method?: string,
@@ -22,38 +21,52 @@ export async function fetchApi(path : string, options: FetchDataOptions){
       TOKEN = authDataTyped.authToken;
     }
   }
-  let formData;
-  if(options.useFormData){
-    formData = new FormData();
-    for (let key in options.data){
-      formData.append(key, options.data[key]);
-    }
-    options.data = formData;
-  }
 
   var bearer = 'Bearer ' + `${TOKEN}`;
-  const configurationObject = {
+  let configurationObject: {
+    method: string,
+    url: string,
+    headers: {
+      authorization?: string,
+      accept: string,
+      'Content-Type'?: string
+    },
+    params?: any,
+    data?: any
+  } = {
     method:(options.method || 'get'),
     url: SERVER_IP+":"+PORT+path,
     headers: {
         authorization : (options.useToken) ? bearer : undefined,
-        "Content-Type": options.useFormData ? "multipart/form-data" : "application/json"
-    },  
+        accept: "application/json",
+      },  
     params: options.params,
-    data: options.useFormData ? undefined : options.data,
-    formData: formData
-    
+    data: options.data,    
   };
 
-  try{
+  if (!options.useFormData) {
+    configurationObject.headers['Content-Type'] = 'application/json';
+  }else{
+    configurationObject.headers['Content-Type'] = 'multipart/form-data';
+  }
+
+  try {
     let response = await axios(configurationObject);
     return response;
-  }catch(err: any){
-    console.error("Problema api: "+err)
-    if (err.status === 401){
-      AsyncStorage.removeItem('authData');
-      if (usePathname().startsWith("/auth/")) return;
-      useRouter().replace("/auth/");    
+  } catch (error: any) {
+    console.error('Network error:', error);
+    if (error && error.response) {
+      // Server responded with a status other than 2xx
+      console.error('Response data:', error.response.data);
+      console.error('Response status:', error.response.status);
+      console.error('Response headers:', error.response.headers);
+    } else if (error.request) {
+      // Request was made but no response received
+      console.error('Request data:', error.request);
+    } else {
+      // Something happened in setting up the request
+      console.error('Error message:', error.message);
     }
-  };
+    throw error;
+  }
 }
