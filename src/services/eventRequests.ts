00e63044623acbeb7ce04a1c314ccd5d0801989b
Andre-lucs <andreltd312@gmail.com>
2024-09-24 13:01:29 -0300

    feat: Add eventrequests service to handle event requests

---
+++ src/services/eventRequests.ts
@@ -0,0 +1,10 @@
+import {fetchApi} from './api'
+
+export function eventCreate(data:any){
+  // return fetchApi('/events', {
+  //   method: 'POST',
+  //   useFormData: true,
+  //   data: data,
+  // })
+
+}
