a0ae5610553489360faea6ba1fb877dba6a4fe47
Andre-lucs <andreltd312@gmail.com>
2024-09-24 20:44:55 -0300

    feat: added event creation, with image upload

--- src/pages/events/EventCreate/index.tsx
+++ src/pages/events/EventCreate/index.tsx
@@ -1,11 +1,17 @@
-import EventCreateForm from "@/src/components/Forms/EventCreateForm";
+import EventCreateForm, { CreateEventResponse } from "@/src/components/Forms/EventCreateForm";
 import StackHeader from "@/src/components/StackHeader";
+import { eventCreate } from "@/src/services/eventRequests";
+import { useRouter } from "expo-router";
 import { ScrollView } from "react-native";
 
 
 export default function (){
-  function handleSubmit(data: any){
-    console.log(data);
+  async function handleSubmit(data: CreateEventResponse){
+    let response = await eventCreate(data);
+    if(response?.status == 201) {
+      alert('Evento criado com sucesso!');
+      useRouter().dismiss()
+    }
   }
 
   return (
--- src/services/eventRequests.ts
+++ src/services/eventRequests.ts
@@ -1,10 +1,33 @@
+import { CreateEventResponse } from '@/src/components/Forms/EventCreateForm';
 import {fetchApi} from './api'
+import * as FileSystem from 'expo-file-system';
 
-export function eventCreate(data:any){
-  // return fetchApi('/events', {
-  //   method: 'POST',
-  //   useFormData: true,
-  //   data: data,
-  // })
+export async function eventCreate(data:CreateEventResponse){
+  let imageUri = data.image;
+
+  let formData = new FormData();
+  formData.append('name', data.name);
+  formData.append('description', data.description);
+  formData.append('endDate', data.endDate.toUTCString());
+  formData.append('startDate', data.startDate.toUTCString());
+  formData.append('location', JSON.stringify({
+    type: 'Point',
+    coordinates: [data.location.latitude, data.location.longitude],
+  }));
+  formData.append('theme', data.theme);
+  formData.append('organizingCommitte', data.committee);
+  formData.append('organizer', "656a4d952f028b43876fda75");
+  formData.append('photo', {
+    uri: imageUri,
+    type: 'image/jpg',
+    name: 'photo.jpg',
+  } as any);
+
+  return fetchApi('/evento', {
+    method: 'POST',
+    useToken: true,
+    useFormData: true,
+    data: formData,
+  })
 
 }
