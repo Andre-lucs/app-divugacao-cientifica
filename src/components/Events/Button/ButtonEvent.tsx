4b449fb1080c34ac15e12e28afe72f3f4d30c25b
Diogo Santana <diogosantanafreitasuna@gmail.com>
2024-09-25 08:00:57 -0300

    fix: adicionando proppriedades em ButtonEvent

--- src/components/Events/Button/ButtonEvent.tsx
+++ src/components/Events/Button/ButtonEvent.tsx
@@ -1,19 +1,28 @@
-import { ButtonEvent, ButtonEventText } from '../../../pages/events';
+import { ButtonEvent, ButtonEventText } from '@/src/pages/events';
 
 type ButtonProps = {
     title: string;
     color?: string;
-    width?: number
+    width?: number;
     onPress?: () => void;
+    disabled?: boolean;
 };
 
-const ButtonEventComponent = ({ title, color, width, onPress }: ButtonProps) => {
+const ButtonEventComponent = ({ title, color, width, onPress, disabled }: ButtonProps) => {
     return (
-        <ButtonEvent onPress={onPress} color={color} width={width}>
+        <ButtonEvent 
+            onPress={!disabled ? onPress : undefined} 
+            color={disabled ? '#d3d3d3' : color} 
+            width={width}
+            disabled={disabled}
+        >
             <ButtonEventText>{title}</ButtonEventText>
         </ButtonEvent>
     );
 };
 
 export default ButtonEventComponent;
+
+
+
 
