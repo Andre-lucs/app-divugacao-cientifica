cab1ca4d2284679f1a0f1457718a6c5ba7bbadcb
Andre-lucs <andreltd312@gmail.com>
2024-09-25 08:38:22 -0300

    feat: add redirect after event creation

--- src/pages/events/EventCreate/index.tsx
+++ src/pages/events/EventCreate/index.tsx
@@ -1,20 +1,25 @@
 import EventCreateForm, { CreateEventResponse } from "@/src/components/Forms/EventCreateForm";
 import StackHeader from "@/src/components/StackHeader";
 import { eventCreate } from "@/src/services/eventRequests";
-import { useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
+import { useState } from "react";
 import { ScrollView } from "react-native";
 
 
 export default function (){
+  const [created, setCreated] = useState(false);
+  
   async function handleSubmit(data: CreateEventResponse){
     let response = await eventCreate(data);
     if(response?.status == 201) {
       alert('Evento criado com sucesso!');
-      useRouter().dismiss()
+      setCreated(true);
     }
   }
 
-  return (
+  if(created) {
+    return <Redirect href={"/user/event"}/>
+  }else return (
     <>
       <StackHeader title="Inscrição de Evento" />
       <ScrollView>
