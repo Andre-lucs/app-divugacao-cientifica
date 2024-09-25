338f5b0014637f8e7d561651487678c1f5d2e2e1
Andre-lucs <andreltd312@gmail.com>
2024-09-25 08:54:46 -0300

    refactor: refactor event creation page

--- src/pages/events/EventCreate/index.tsx
+++ src/pages/events/EventCreate/index.tsx
@@ -1,30 +1,40 @@
 import EventCreateForm, { CreateEventResponse } from "@/src/components/Forms/EventCreateForm";
 import StackHeader from "@/src/components/StackHeader";
 import { eventCreate } from "@/src/services/eventRequests";
-import { Redirect, useRouter } from "expo-router";
-import { useState } from "react";
+import { Redirect, usePathname, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
 
-
-export default function (){
+export default function () {
   const [created, setCreated] = useState(false);
-  
-  async function handleSubmit(data: CreateEventResponse){
+  const pathname = usePathname();
+  async function handleSubmit(data: CreateEventResponse) {
     let response = await eventCreate(data);
-    if(response?.status == 201) {
+    if (response?.status == 201) {
       alert('Evento criado com sucesso!');
       setCreated(true);
     }
   }
 
-  if(created) {
-    return <Redirect href={"/user/event"}/>
-  }else return (
-    <>
-      <StackHeader title="Inscrição de Evento" />
-      <ScrollView>
-        <EventCreateForm onSubmit={handleSubmit} />
-      </ScrollView>
-    </>
-  );
+  useEffect(() => {
+    // Redefine o estado created quando o componente é desmontado
+    return () => {
+      setCreated(false);
+    };
+  }, [pathname]);
+
+  if (created) {
+    return (
+      <Redirect href={"/user/event"} />
+    );
+  } else {
+    return (
+      <>
+        <StackHeader title="Inscrição de Evento" />
+        <ScrollView>
+          <EventCreateForm onSubmit={handleSubmit} />
+        </ScrollView>
+      </>
+    );
+  }
 }
